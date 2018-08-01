ce18f04acd807a0552f03c8fa48966b7175b4d9a
Rose <thebarbreaker@gmail.com>
2018-08-01 00:04:05 -0400

    map lines to feed_ids

--- mtaAPI.js
+++ mtaAPI.js
@@ -1,15 +1,40 @@
 const Mta = require('mta-gtfs');
+const feed_ids = require('./feed_ids');
+const { key } = require('./config');
 
-const mta = new Mta({
-  key: require("./config").key,
-  feed_id: 26
-});
+const mta = new Mta({key});
 
-module.exports = {
-  getAllStops: () => mta.stop(),
-  getOneStop: stopId => mta.stop(stopId),
-  getSchedule: () => {
-
+const MTA_MODEL = {
+  getStop: stopId => mta.stop(stopId),  // omit stopId to get all stops
+  getStatus: serviceType => mta.status(serviceType),  // omit service type to get all services (subway, bus, BT, LIRR, MetroNorth)
+  getSchedule: stopId => {
+    
   },
 
 }
+
+module.exports = MTA_MODEL;
+
+/* Example calls
+MTA_MODEL.getStop()
+.then(res => console.log(res))
+.catch(err => console.err(err));
+
+*/
+
+// MTA_MODEL.getStatus()
+// .then(res => console.log(res))
+// .catch(err => console.err(err));
+
+// mta.schedule('635', 1).then(function (result) {
+//   console.log('635', result);
+// });
+
+// mta.schedule('A31', 26).then(result =>console.log(result));
+
+
+// mta.schedule(['635', 'A31'], [1, 26]).then(result =>console.log(result));
+
+// MTA_MODEL.getStop(635)
+// .then(res => console.log(res))
+// .catch(err => console.err(err));
